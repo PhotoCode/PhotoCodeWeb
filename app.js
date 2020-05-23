3a3e48533ee1bae089a3f0c42f2431f4c6a4a323
GeeTransit <geetransit@gmail.com>
2020-05-23 15:24:08 -0400

    Add development tools
    Add nodemon
    Use port 3000 if environment variable PORT is not defined

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 	res.send("It works");
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
