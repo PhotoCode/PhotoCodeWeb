3eb60ccda849e5875159cc45b5170899cca2d2cc
GeeTransit <geetransit@gmail.com>
2020-05-23 18:58:21 -0400

    Might work?

--- app.js
+++ app.js
@@ -3,32 +3,31 @@ const multer  = require('multer');
 const upload = multer();
 const vision = require('@google-cloud/vision');
 const axios = require('axios');
-const client = new vision.ImageAnnotatorClient();
 
 const app = express();
 const client = new vision.ImageAnnotatorClient();
+const HACKEREARTH_RUN = "https://api.hackerearth.com/v3/code/run/";
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send("It works");
 });
 
-const RUN_LINK = "https://api.hackerearth.com/v3/code/run/";
-
 app.post('/run', async (req, res) => {
-	let options = {
-		"client_secret": process.env.HACKEREARTH_SECRET,
+	const config = {
+		"config_secret": process.env.HACKEREARTH_SECRET,
 		"source": req.body.code,
 		"lang": "JAVASCRIPT"
 	};
-  try {
-    const response = await axios.post(RUN_LINK, options);
+	try {
+    const response = await axios.post(HACKEREARTH_RUN, config);
     const {message, errors, run_status: {output}} = response;
 		const result = {"output": output, "message": message, "errors": errors};
 		console.log(result);
     res.json(result);
   } catch (error) {
-		const result = {"error": error};
+		const result = {"errors": {[error.message]: error}};
 		console.log(result);
     res.json(result);
   }
