435f8fbf1f9d74e5f127c6da5764b0af9458fd4a
GeeTransit <geetransit@gmail.com>
2020-05-23 15:47:38 -0400

    Add JSON body parsing
    Add express.json() middleware
    Add /run POST

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send("It works");
+});
+
+app.post('/run', (req, res) => {
+	res.send(req.body.code);
 });
 
 app.listen(process.env.PORT || 3000);
