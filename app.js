8b75b900737782c7259b6c0938a21b8cf146e4da
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2020-05-23 16:03:04 -0400

    added OCR API functionality

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const vision = require('@google-cloud/vision');
 
 const app = express();
+const client = new vision.ImageAnnotatorClient();
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -9,6 +12,14 @@ app.get('/', (req, res) => {
 
 app.post('/run', (req, res) => {
 	res.send(req.body.code);
+
+});
+
+app.post('/scan', (req, res) => {
+	const [result] = await client.documentTextDetection(req.body.image);
+	const fullTextAnnotation = result.fullTextAnnotation;
+	
+	res.send(fullTextAnnotation.text);
 });
 
 app.listen(process.env.PORT || 3000);
