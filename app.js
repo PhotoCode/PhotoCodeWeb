b5edee5dffab8bd268161af8b18561fbc94e97ae
GeeTransit <geetransit@gmail.com>
2020-05-24 04:25:28 -0400

    Big update
    Add image uploading
    Add text recognition
    Removed Herobrine

--- app.js
+++ app.js
@@ -5,37 +5,46 @@ const axios = require("axios");
 const path = require("path");
 const qs = require("querystring");
 
+/*
+ENVIRONMENT VARIABLES
+GOOGLE_SECRET: json of options (has secret key) # https://googleapis.dev/nodejs/vision/latest/v1.ImageAnnotatorClient.html#ImageAnnotatorClient
+HACKEREARTH_SECRET: secret key for code api
+IMGBB_SECRET: secret key for image api
+*/
+
+// app setup
 const app = express();
-const client = new vision.ImageAnnotatorClient();
+const upload = multer();
+
+// google cloud setup
+const options = JSON.parse(process.env.GOOGLE_SECRET);
+const client = new vision.ImageAnnotatorClient(options);
+
+// constants
 const HACKEREARTH_RUN = "https://api.hackerearth.com/v3/code/run/";
 const HACKEREARTH_SECRET = process.env.HACKEREARTH_SECRET;
+const IMGBB_UPLOAD = "https://api.imgbb.com/1/upload";
+const IMGBB_SECRET = process.env.IMGBB_SECRET;
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
-  }
-});
-
-const upload = multer({ storage: storage });
-
+// middleware
 app.use(express.json());
 app.use(express.static("public"));
 
+// main (temporary) page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// run specified code
+// body: {code: string}
 app.post("/run", async (req, res) => {
-  console.log("Code:", req.body.code);
+	// prepare query
   const stringifiedData = qs.stringify({
     client_secret: HACKEREARTH_SECRET,
     source: req.body.code,
     lang: "JAVASCRIPT_NODE"
   });
-
+	// try getting result of code
   try {
     const {data} = await axios.post(HACKEREARTH_RUN, stringifiedData);
     const result = {message: data.message, errors: data.errors};
@@ -50,29 +59,43 @@ app.post("/run", async (req, res) => {
 		}
     return res.json(result);
   } catch (error) {
+		// something oofed
     console.log("Error:", error.message);
     if (error.response) return res.json(error.response.data);
     return res.json(error);
   }
 });
 
-app.post("/scan", upload.single("image"), (req, res) => {
-  res.json({
-    data: `
-function add(a, b) {
-	return a+b;
-}
-console.log(add(1, 2));
-`
-  });
-  /*
-  client
-    .documentTextDetection(path.join(__dirname, req.file.path))
-    .then(result => {
-      res.json({ result });
-    })
-    .catch(err => console.log(err));
-    */
+// scan, upload, and return image info
+// file: image
+app.post("/scan", upload.single("image"), async (req, res) => {
+	const buffer = req.file.buffer;
+	try {
+		const imageURL = await uploadImage(buffer);
+		const code = await scanImage(buffer);
+		return res.json({imageURL, code});
+	} catch (error) {
+		if (error.response) return res.json(error.response.data);
+		return res.json(error);
+	}
 });
 
+// pass base 64 of image
+async function uploadImage(buffer) {
+	const stringifiedData = qs.stringify({
+		key: IMGBB_SECRET,
+		image: buffer.toString("base64")
+	});
+	const response = await axios.post(IMGBB_UPLOAD, stringifiedData);
+	return response.data.data.url;
+}
+
+// pass url of image # https://googleapis.dev/nodejs/vision/latest/v1.ImageAnnotatorClient.html#textDetection
+async function scanImage(buffer) {
+	// const request = {image: {content: buffer}};
+  const [result] = await client.textDetection(buffer);
+	return result.fullTextAnnotation.text;
+}
+
+// start it up :D
 app.listen(process.env.PORT || 3000, _ => console.log("App started"));
