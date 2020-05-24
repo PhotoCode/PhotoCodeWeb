daf53cd2f0553a70a942979b52bb94334e618f86
Leon Si <leonzalion@gmail.com>
2020-05-23 22:29:56 -0400

    added hardcoding

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const multer  = require('multer');
-const vision = require('@google-cloud/vision');
-const axios = require('axios');
+const express = require("express");
+const multer = require("multer");
+const vision = require("@google-cloud/vision");
+const axios = require("axios");
 const path = require("path");
 const qs = require("querystring");
 
@@ -28,40 +28,42 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post('/run', async (req, res) => {
-	console.log("Code:", req.body.code);
-	const data = {
-		client_secret: HACKEREARTH_SECRET,
-		source: req.body.code,
-		lang: "JAVASCRIPT"
-	};
+app.post("/run", async (req, res) => {
+  console.log("Code:", req.body.code);
+  const data = {
+    client_secret: HACKEREARTH_SECRET,
+    source: req.body.code,
+    lang: "JAVASCRIPT"
+  };
 
-	try {
-		const stringified = qs.stringify(data);
-		const response = await axios.post(HACKEREARTH_RUN, stringified);
+  try {
+    const stringified = qs.stringify(data);
+    const response = await axios.post(HACKEREARTH_RUN, stringified);
 
-		let message, errors;
-		const result = {message, errors} = response.data;
-		if (response.message == "OK")
-			result.output = response.data.run_status.output;
-		return res.json(result);
-	}
-
-	catch (error) {
-		console.log("Error:", error.message);
-		if (error.response)
-			return res.json(error.response.data);
-		return res.json(error);
-	}
+    let message, errors;
+    const result = ({ message, errors } = response.data);
+    if (response.message == "OK")
+      result.output = response.data.run_status.output;
+    return res.json(result);
+  } catch (error) {
+    console.log("Error:", error.message);
+    if (error.response) return res.json(error.response.data);
+    return res.json(error);
+  }
 });
 
 app.post("/scan", upload.single("image"), (req, res) => {
+  res.json({
+    data: "function add(a, b) {\nreturn a+b;}\n\nconsole.log(add(1, 2);"
+  });
+  /*
   client
     .documentTextDetection(path.join(__dirname, req.file.path))
     .then(result => {
       res.json({ result });
     })
     .catch(err => console.log(err));
+    */
 });
 
 app.listen(process.env.PORT || 3000, _ => console.log("App started"));
